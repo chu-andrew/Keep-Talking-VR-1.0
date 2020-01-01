67bb8771f6284b646b3208dccd9e0aa74d82a831
daniel-lee-user <danielhlee02@gmail.com>
2020-01-01 12:55:31 -0800

    hideSpeechToText
    Able to hide speech to text window in game editor (uncheck enabled in speechtotext gameobject)

---
+++ Assets/hideSpeechToText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideSpeechToText : MonoBehaviour {
+    public GameObject hide;
+    public bool enabled;
+    // Use this for initialization
+    void Start () {
+		if(!enabled) {
+            hide.SetActive(false);
+        } else {
+            hide.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
