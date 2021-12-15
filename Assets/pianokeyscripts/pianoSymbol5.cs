c6b12f8a56dc138dd0f6f9fe196d9b5923eaa5df
chu-andrew <chu.andrew.h@gmail.com>
2021-12-15 01:19:54 -0800

    added real images and musical sequences, changed highlighting

---
+++ Assets/pianokeyscripts/pianoSymbol5.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// copied this class from sym1.cs in symbolscripts
+public class pianoSymbol5 : MonoBehaviour
+{
+    public GameObject myObject;
+    public GameObject pianoImage5;
+    RectTransform m_RectTransform;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_RectTransform = GetComponent<RectTransform>();
+        //Initiate the x and y positions
+        if (myObject.GetComponent<pianokeyscontroller>().image == 5)
+        {
+            pianoImage5.SetActive(true);
+        }
+        else
+        {
+            pianoImage5.SetActive(false);
+        }
+    }
+}
